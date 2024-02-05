908d392d395eb79d64cd2e34b62e9458373a6f9e
usagi <kou1992@gmail.com>
2024-02-05 19:06:32 +0900

    build

--- docs/.lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const buildTextLintCommand = (filenames) => `textlint ${filenames
-  .map((f) => path.relative(process.cwd(), f))
-  .join(' ')}`
-
-module.exports = {
-  "docs/**/*.md": [buildTextLintCommand]
-}
